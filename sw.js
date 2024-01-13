301b8261fa76ba31f194be7f8622f85a36132564
garvshah07 <garv8890@gmail.com>
2024-01-13 14:27:35 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,8 +2,8 @@ const CACHE_NAME = "todo-app-cache-v1";
 const urlsToCache = [
   "/",
   "/index.html",
-  "/todo.css",
-  "/todo.js",
+  "/ToDo.css",
+  "/ToDo.js",
   "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css",
   "https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Roboto+Flex:opsz,wght@8..144,100;8..144,200;8..144,300;8..144,400;8..144,500;8..144,600;8..144,700;8..144,800;8..144,900;8..144,1000&display=swap",
 ];
