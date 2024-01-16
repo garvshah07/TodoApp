7ce9ee27f3956622d4146154fd5981f3a8c76375
garvshah07 <garv8890@gmail.com>
2024-01-16 17:27:20 +0530

    commit

--- ToDo.js
+++ ToDo.js
@@ -6,7 +6,7 @@ const card = document.querySelector(".card");
 const noteInput = document.createElement("input");
 noteInput.type = "text";
 noteInput.name = "inputNote";
-noteInput.placeholder = "Add a task..";
+noteInput.placeholder = "Please enter a task..";
 noteInput.classList.add("textEdit");
 
 noteCantainer.insertAdjacentElement("afterbegin", noteInput);
